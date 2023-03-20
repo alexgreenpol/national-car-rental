3b93579f9d5e21626c49d9bc40503bd866b9be2e
sukhrob.sanhinov <sukhrob.sanhinov@codeit.pro>
2023-03-20 20:16:40 +0200

    Init project and upload on github

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# national-car-rental
+<h2>About the project</h2>
+
+  <p>A <b>car rental</b> website which was based on react.</p>
+
+👉 Live Demo: <a href='https://ag-car-rental.vercel.app/'>Live Demo</a>
+
+<h3>Build with:</h3>
+
+» Sass / Scss <br>
+» React JS
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles/styles.scss";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Navbar from "../src/components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Models from "./pages/Models";
+import TestimonialsPage from "./pages/TestimonialsPage";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route index path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="models" element={<Models />} />
+				<Route path="testimonials" element={<TestimonialsPage />} />
+				<Route path="team" element={<Team />} />
+				<Route path="contact" element={<Contact />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
